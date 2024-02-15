33960417fb8edd74e7ad416af95f12696d947edf
abhilasha <abhilashaduwal@gmail.com>
2024-02-15 22:53:18 +0545

    feat(node-app): add sql queries and test database

---
+++ node-app/db/mysql.sql
@@ -0,0 +1,11 @@
+-- CREATE TABLE tasks (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     title TEXT NOT NULL,
+--     description TEXT NOT NULL,
+--     completed INTEGER DEFAULT 0 NOT NULL
+-- );
+
+-- DROP TABLE TASKS;
+
+-- INSERT INTO tasks (title, description)
+-- VALUES ("second task", "Description for Second Task" );  
